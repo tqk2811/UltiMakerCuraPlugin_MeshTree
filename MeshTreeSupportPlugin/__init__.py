b5b079b4a3d30709221f29fe1f122d9dbbd6480e
tqk2811 <tqk2811@gmail.com>
2026-03-17 14:15:39 +0700

    delete all

--- MeshTreeSupportPlugin/__init__.py
+++
@@ -1,9 +0,0 @@
-from .MeshTreeSupportExtension import MeshTreeSupportExtension
-
-
-def getMetaData() -> dict:
-    return {}
-
-
-def register(app) -> dict:
-    return {"extension": MeshTreeSupportExtension()}
